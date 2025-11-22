e86c959d743dab8957349fdfa2694dd1d12c9ca1
Hana Go <46773382+devhaaana@users.noreply.github.com>
2025-11-22 18:50:42 +0900

    [Add] v1.0.0

---
+++ components/PageAside.tsx
@@ -0,0 +1,29 @@
+import { type Block, type ExtendedRecordMap } from 'notion-types'
+
+import { getPageTweet } from '@/lib/get-page-tweet'
+
+import { PageActions } from './PageActions'
+
+export function PageAside({
+  block,
+  recordMap,
+  isBlogPost
+}: {
+  block: Block
+  recordMap: ExtendedRecordMap
+  isBlogPost: boolean
+}) {
+  if (!block) {
+    return null
+  }
+
+  // only display comments and page actions on blog post pages
+  if (isBlogPost) {
+    const tweet = getPageTweet(block, recordMap)
+    if (!tweet) {
+      return null
+    }
+
+    return <PageActions tweet={tweet} />
+  }
+}
---
+++ lib/page-ids.ts
@@ -0,0 +1,4 @@
+export const WIDE_PAGE_IDS = [
+  '2aa23a07-c536-8080-b473-f214d8871873', // paper archive
+  '2aa23a07-c536-8058-981b-cff1308b08d8' // project archive
+]
